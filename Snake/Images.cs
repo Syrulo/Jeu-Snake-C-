51214670d0c8560551cb378284a7aa53604fb53e
Thomas <tborestel@gmail.com>
2024-04-27 08:41:13 +0200

    first commit

---
+++ Snake/Images.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Snake
+{
+    public static class Images
+    {
+        public readonly static ImageSource Empty = loadImage("Empty.png");
+        public readonly static ImageSource Body = loadImage("Body.png");
+        public readonly static ImageSource Head = loadImage("Head.png");
+        public readonly static ImageSource Food = loadImage("Food.png");
+        public readonly static ImageSource DeadBody = loadImage("DeadBody.png");
+        public readonly static ImageSource DeadHead = loadImage("DeadHead.png");
+
+        private static ImageSource loadImage(string fileName)
+        {
+            return new BitmapImage(new Uri($"Assets/{fileName}", UriKind.Relative));
+        }
+    }
+}
